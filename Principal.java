0c96b2a6d965fef2ed8b7a8c7847e7db15471f73
carlam97 <carmartins04@hotmail.com>
2021-09-09 19:38:44 +0000

    conta e valores

--- Principal.java
+++ Principal.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
 public class Principal{
     public static void main(String args[]){
 
+        Scanner ler=new Scanner(System.in);
         Conta minhaConta=new Conta();
+
+       // ler.netLine()         //texto
+       // ler.nextIn()          //inteiro
+       // ler.nextDouble()      //double
+        System.out.println("Digite o nome do titular: ");
+        minhaConta.titular=ler.nextLine();
+        System.out.println("Digite o número da conta: ");
+        minhaConta.numero=ler.nextInt();
+        System.out.println("Digite o saldo disponível: ");
+        minhaConta.saldo=ler.nextDouble();
         
+        //minhaConta.numero=12345;
+        //minhaConta.saldo=20;
 
-        minhaConta.numero=12345;
-        minhaConta.saldo=20;
-
-    System.out.println("O numero da conta é " + minhaConta.numero);
-    System.out.println("O saldo da conta é " + minhaConta.saldo);
+    System.out.println("Senhor(a)" + minhaConta.titular + ". O numero da sua respectiva conta é " 
+    + minhaConta.numero + ". Cujo saldo disponível é equivalente à " + minhaConta.saldo);
+    
         
         
     }
